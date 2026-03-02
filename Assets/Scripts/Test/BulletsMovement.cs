000061c6466f9e238515f7101f50711d2a57bd7e
Miguel57835 <micalder@ucm.es>
2026-03-02 19:56:41 +0100

    MovimientoProyectiles
    - Componente de movimiento de proyectiles
    - Componente de DeadZone que elimina proyectiles con el OntriggerExit2D
    - Escena de testing para bullets

---
+++ Assets/Scripts/Test/BulletsMovement.cs
@@ -0,0 +1,79 @@
+//---------------------------------------------------------
+// Componente que describe el movimiento de un proyectil recto
+// Miguel Calderón Barba
+// Dream O’ SpaceSheep
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UIElements;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Movimiento de proyectil recto
+/// </summary>
+public class BulletsMovement : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    [SerializeField]
+    private float Velocidad = 6f; //El número de unidades de unity que recorre por cada frame
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        transform.position += new Vector3 (-1*(Velocidad * Time.deltaTime),0,0); //Movimiento Rectilineo Uniforme hacia -i;
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+    public void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class BulletsMovement 
+// namespace
---
+++ Assets/Scripts/Test/DeadZone.cs
@@ -0,0 +1,75 @@
+//---------------------------------------------------------
+// Componente que gestiona la reacción ante impactos
+// Miguel Calderón Barba
+// Dream O’ SpaceSheep
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Gestiona la reacción ante las diferentes colisiones que pueden
+/// impactar con la entidad y llama a las funciones necesarias 
+/// de las entidades impactadas.
+/// </summary>
+public class DeadZone : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+    #endregion
+    
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+    
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<BulletsMovement>() != null)
+        {
+            //Debug.Log("Me ha dado algo que tengo que destruir");
+            collision.TryGetComponent<BulletsMovement>(out BulletsMovement bullet);
+            bullet.DestroyBullet();
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion
+
+} // class DeadZone 
+// namespace
