d223c83fee4b85b0e3c00c17349610cc470325c1
Riuttasu <239182073+Riuttasu@users.noreply.github.com>
2026-03-13 17:22:55 +0100

    Escudo del jugador
    Pickup escudo, script del escudo, el escudo es pickable, escudo se relaciona con la vida, y hace ataque.

--- Assets/Scripts/Test/Pickable.cs
+++ Assets/Scripts/Test/Pickable.cs
@@ -23,7 +23,7 @@ public class Pickable : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
-    
+
     [SerializeField]
     private PickableType PickUp;
 
@@ -59,8 +59,8 @@ public class Pickable : MonoBehaviour
         // muchas cosas están comentadas porque aún no se han implementado
         if (collider.GetComponent<PlayerControler>() != null) // si el collider que ha entrado es el del jugador
         {
-            //switch (PickUp)
-            //{
+            switch (PickUp)
+            {
                 /*case PickableType.GanaVida:
                     // se llama al componente concreto que tendrá la lógica de este pickup
                     if (collider.GetComponent<PickUpVida>() != null)
@@ -73,32 +73,33 @@ public class Pickable : MonoBehaviour
                         Debug.LogWarning("No está el componente PickUpVida, no se puede aplicar el efecto de este pickup");
                     }
                     break;*/
-                /*case PickableType.Escudo:
-                    // se llama al componente concreto que tendrá la lógica de este pickup
-                    if (collider.GetComponent<PickUpEscudo>() != null)
+                case PickableType.Escudo:
+                    // Se busca el componente escudo dentro del player
+                    PlayerShield ps = collider.GetComponent<PlayerShield>();
+                    if (ps != null)
                     {
-                        // collider.GetComponent<PickUpVida>().ActivateShield; // esto es un ejemplo
-                        // se llama al método que activa el escudo (aún no está hecho)
+                        // Se activa el escudo
+                        ps.EnableShield();
                     }
                     else
                     {
                         Debug.LogWarning("No está el componente PickUpEscudo, no se puede aplicar el efecto de este pickup");
                     }
-                    break;*/
-                /*case PickableType.ReduceTamanyo:
-                    // se llama al componente concreto que tendrá la lógica de este pickup
-                    if (collider.GetComponent<PickUpSmall>() != null)
-                    {
-                        // collider.GetComponent<PickUpSmall>().ReduceSize(); // esto es un ejemplo
-                        // se llama al método que cura (aún no está hecho)
-                    }
-                    else
-                    {
-                        Debug.LogWarning("No está el componente PickUpSmall, no se puede aplicar el efecto de este pickup");
-                    }
-                    break;*/
-            //}
-            DestroyPickUp(); // se destruye el pickup después de activar su efecto
+                    DestroyPickUp(); // se destruye el pickup después de activar su efecto
+                    break;
+                    /*case PickableType.ReduceTamanyo:
+                        // se llama al componente concreto que tendrá la lógica de este pickup
+                        if (collider.GetComponent<PickUpSmall>() != null)
+                        {
+                            // collider.GetComponent<PickUpSmall>().ReduceSize(); // esto es un ejemplo
+                            // se llama al método que cura (aún no está hecho)
+                        }
+                        else
+                        {
+                            Debug.LogWarning("No está el componente PickUpSmall, no se puede aplicar el efecto de este pickup");
+                        }
+                        break;*/
+            }
         }
     }
 
@@ -116,7 +117,7 @@ public class Pickable : MonoBehaviour
     /// <summary>
     /// se destruye el pickup después de activar su efecto
     /// </summary>
-    void DestroyPickUp()
+    private void DestroyPickUp()
     {
         Destroy(gameObject); // adiós pookies
     }
