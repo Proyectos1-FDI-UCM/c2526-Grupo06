9d9fac33c57e819790eb7712420f1141384dbbab
Riuttasu <239182073+Riuttasu@users.noreply.github.com>
2026-04-29 12:53:43 +0200

    Quito debug.log

--- Assets/AnimatedBackgroundSystem.cs
+++ Assets/AnimatedBackgroundSystem.cs
@@ -11,18 +11,12 @@ using UnityEngine;
 
 
 /// <summary>
-/// Antes de cada class, descripción de qué es y para qué sirve,
-/// usando todas las líneas que sean necesarias.
+/// Sistema que controla los elementos del fondo.
 /// </summary>
 public class AnimatedBackgroundSystem : MonoBehaviour
 {
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
-    // Documentar cada atributo que aparece aquí.
-    // El convenio de nombres de Unity recomienda que los atributos
-    // públicos y de inspector se nombren en formato PascalCase
-    // (palabras con primera letra mayúscula, incluida la primera letra)
-    // Ejemplo: MaxHealthPoints
     [SerializeField] Transform BackgroundPage1;
     [SerializeField] Transform BackgroundPage2;
     [SerializeField] SpriteRenderer BackgroundSprite1;
@@ -35,21 +29,11 @@ public class AnimatedBackgroundSystem : MonoBehaviour
 
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
-    // Documentar cada atributo que aparece aquí.
-    // El convenio de nombres de Unity recomienda que los atributos
-    // privados se nombren en formato _camelCase (comienza con _, 
-    // primera palabra en minúsculas y el resto con la 
-    // primera letra en mayúsculas)
-    // Ejemplo: _maxHealthPoints
     private float _spawnHeight = -5;
     #endregion
     
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
-    
-    // Por defecto están los típicos (Update y Start) pero:
-    // - Hay que añadir todos los que sean necesarios
-    // - Hay que borrar los que no se usen 
     
     /// <summary>
     /// Start is called on the frame when a script is enabled just before 
@@ -78,27 +62,16 @@ public class AnimatedBackgroundSystem : MonoBehaviour
         }
         BackgroundPage1.position += new Vector3(-BackgroundSpeed * Time.deltaTime, BackgroundSpeed/(4) * Time.deltaTime, 0);
         BackgroundPage2.position += new Vector3(-BackgroundSpeed * Time.deltaTime, BackgroundSpeed/(4) * Time.deltaTime, 0);
-        Debug.Log(1/2 * BackgroundSpeed);
     }
     #endregion
 
     // ---- MÉTODOS PÚBLICOS ----
     #region Métodos públicos
-    // Documentar cada método que aparece aquí con ///<summary>
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-    // Ejemplo: GetPlayerController
 
     #endregion
     
     // ---- MÉTODOS PRIVADOS ----
     #region Métodos Privados
-    // Documentar cada método que aparece aquí
-    // El convenio de nombres de Unity recomienda que estos métodos
-    // se nombren en formato PascalCase (palabras con primera letra
-    // mayúscula, incluida la primera letra)
-
     #endregion   
 
 } // class AnimatedBackgroundSystem 
